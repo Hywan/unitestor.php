a956ac07793778a1d780fd66ba3b2a19b33e36b6
Ivan Enderlin <ivan.enderlin@hoa-project.net>
2013-09-05 14:27:06 +0200

    Prepare step1.

--- Test/UniTestor/Coordinates.php
+++ Test/UniTestor/Coordinates.php
@@ -9,27 +9,7 @@ class Coordinates extends \Test {
         $x = 7.0;
         $y = 42.0;
 
-        $this
-            ->if($coordinates = new \UniTestor\Coordinates($x, $y))
-            ->then
-                ->float($coordinates->getX())
-                    ->isEqualTo($x)
-                ->float($coordinates->getY())
-                    ->isEqualTo($y)
-            ;
-    }
-
-    public function testCast ( ) {
-
-        $x = 7;
-        $y = 42;
-
-        $this
-            ->if($coordinates = new \UniTestor\Coordinates($x, $y))
-            ->then
-                ->float($coordinates->getX())
-                ->float($coordinates->getY())
-            ;
+        // À vous de jouer !
     }
 }
 
--- Test/UniTestor/Vector.php
+++ Test/UniTestor/Vector.php
@@ -6,43 +6,13 @@ class Vector extends \Test {
 
     public function testC1C2 ( ) {
 
-        $c1 = new \UniTestor\Coordinates(0, 0);
-        $c2 = new \UniTestor\Coordinates(0, 0);
-
-        $this
-            ->if($vector = new \UniTestor\Vector($c1, $c2))
-            ->then
-                ->object($vector->getFirstCoordinates())
-                    ->isIdenticalTo($c1)
-                ->object($vector->getSecondCoordinates())
-                    ->isIdenticalTo($c2)
-            ;
+        // À vous de jouer !
     }
 
     public function testLengthSimple ( ) {
 
-        $this
-            ->if($vector = new \UniTestor\Vector(
-                new \UniTestor\Coordinates(0, 0),
-                new \UniTestor\Coordinates(3, 4)
-            ))
-            ->then
-                ->float($vector->getLength())
-                    ->isEqualTo(5.0);
-            ;
-    }
-
-    public function testLength ( ) {
-
-        $this
-            ->if($vector = new \UniTestor\Vector(
-                new \UniTestor\Coordinates(0, 0),
-                new \UniTestor\Coordinates(3, 2)
-            ))
-            ->then
-                ->float($vector->getLength())
-                    ->isNearlyEqualTo(3.605551275464)
-            ;
+        // À vous de jouer !
+        // Indice : distance entre (0, 0) et (3, 4) = 5.
     }
 }
 
