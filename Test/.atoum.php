650862c87283e1b4ccbf8304cfcf7b30c788e2da
Ivan Enderlin <ivan.enderlin@hoa-project.net>
2013-09-10 15:48:02 +0200

    Be compatible with recent atoum modifications.

--- Test/.atoum.php
+++ Test/.atoum.php
@@ -2,4 +2,4 @@
 
 $report = $script->addDefaultReport();
 $runner->setBootstrapFile(__DIR__ . DIRECTORY_SEPARATOR .  '.bootstrap.atoum.php');
-$script->addTestAllDirectory(__DIR__ . DIRECTORY_SEPARATOR);
+$runner->addTestsFromDirectory(__DIR__ . DIRECTORY_SEPARATOR);
